74b1b7559e141a7a799c0de44db852dfa3ec0af2
itsOCDowd <89500206+itsOCDowd@users.noreply.github.com>
2021-10-03 10:15:14 -0700

    Update security.py

--- security.py
+++ security.py
@@ -2,6 +2,9 @@ from flask import Flask
 from flask import Flask, redirect, url_for, render_template, request, flash
 import os
 import random
+import json
+import requests
+import re
 
 app = Flask(__name__)
 
@@ -9,6 +12,11 @@ global links
 links = []
 
 @app.route("/", methods=['GET', 'POST'])
+def home():
+    return render_template('index.html')
+
+
+@app.route("/data/", methods=['GET', 'POST'])
 def index():
     os.chdir('linksbot')
     os.chdir('linksbot')
@@ -25,7 +33,41 @@ def index():
 def urllookup():
     urlchosen = random.choice(links)
     links.remove(urlchosen)
-    return render_template('home.html', video_feed=urlchosen)
+    if request.method == 'POST':
+        favorite = request.form['favorite']
+        print(favorite)
+    start = '//'
+    end = ':'
+    s = urlchosen
+    urlforlookup = s[s.find(start)+len(start):s.rfind(end)]
+    print(urlforlookup)
+    getinfourl = 'https://ipapi.co/' + urlforlookup + '/json'
+    response = requests.get(getinfourl)
+    data = response.json()
+    print(data)
+    country=data['country_name']
+    region=data['region']
+    city=data['city']
+    return render_template('home.html', video_feed=urlchosen, Country=country, Region=region, City=city)
+
+@app.route("/addfavorite/", methods=['GET', 'POST'])
+def addfavorite():
+    favorite = request.form['favorite']
+    print(favorite)
+    with open("favorites.txt", "a") as favoritestxt:
+        favoritestxt.write(favorite + '\n')
+    return render_template('home.html', video_feed=favorite)
+
+@app.route("/favorites/", methods=['GET', 'POST'])
+def viewfavorite():
+    favorites = []
+    with open('favorites.txt','r') as favoritestxt:
+        linksintxt = favoritestxt.readlines()
+        for line in linksintxt:
+            favorites.append(line)
+    currentchoice = random.choice(favorites)
+    print(currentchoice)
+    return render_template('favorites.html', video_feed=currentchoice)
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port='5000', debug=True)
