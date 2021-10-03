016c0e9be482dc3b8c706ce9cf7df9c36b743426
itsOCDowd <89500206+itsOCDowd@users.noreply.github.com>
2021-10-03 07:56:07 -0700

    Update security.py

--- security.py
+++ security.py
@@ -12,22 +12,20 @@ links = []
 def index():
     os.chdir('linksbot')
     os.chdir('linksbot')
-    #os.chdir('linksbot')
     os.system('scrapy crawl linkspider')
 
     with open('links.txt','r') as f:
         lines = f.readlines()
         for line in lines:
             links.append(line)
-    #destination = random.choice(links)
-    #return redirect(url_for(urllookup, url=destination))
+
     return redirect(url_for('urllookup'))
-    #return render_template('home.html', video_feed=random.choice(links))
 
 @app.route("/url/", methods=['GET', 'POST'])
 def urllookup():
-    return render_template('home.html', video_feed=random.choice(links))
+    urlchosen = random.choice(links)
+    links.remove(urlchosen)
+    return render_template('home.html', video_feed=urlchosen)
 
-# main driver function
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port='5000', debug=True)
