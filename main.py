ede0410395c85f6251fa0093b1c2e9bf4fcaeda7
IAmNotPolitical2020 <89500206+IAmNotPolitical2020@users.noreply.github.com>
2021-10-05 17:21:01 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-#Opens dependencies
 from flask import Flask
 from flask import Flask, redirect, url_for, render_template, request, flash
 import os
@@ -10,19 +9,17 @@ import webview
 import sys
 import threading
 import keyboard
-
+import time
 
 app = Flask(__name__)
 
 global links
 links = []
 
-#Route for directory
 @app.route("/", methods=['GET', 'POST'])
 def home():
     return render_template('index.html')
 
-#Route for initial web scraping and redirect to first random security camera
 @app.route("/data/", methods=['GET', 'POST'])
 def index():
     for i in range(1):
@@ -36,7 +33,6 @@ def index():
         return redirect(url_for('urllookup'))
     return 'loading data'
 
-#Route picks a random security camera from web scraping data and displays it on the screen
 @app.route("/url/", methods=['GET', 'POST'])
 def urllookup():
     urlchosen = random.choice(links)
@@ -53,14 +49,18 @@ def urllookup():
     response = requests.get(getinfourl)
     data = response.json()
     print(data)
-    country=data['country_name']
-    region=data['region']
-    city=data['city']
+    try:
+        country=data['country_name']
+        region=data['region']
+        city=data['city']
+    except:
+        country = ''
+        region = ''
+        city = ''
     with open("viewed.txt", "a") as viewedtxt:
         viewedtxt.write(urlchosen)
     return render_template('home.html', video_feed=urlchosen, Country=country, Region=region, City=city)
 
-#Route for adding a new favorite security camera
 @app.route("/addfavorite/", methods=['GET', 'POST'])
 def addfavorite():
     favorite = request.form['favorite']
@@ -69,7 +69,6 @@ def addfavorite():
         favoritestxt.write(favorite + '\n')
     return render_template('home.html', video_feed=favorite)
 
-#Route for viewing favorite security cameras
 @app.route("/favorites/", methods=['GET', 'POST'])
 def viewfavorite():
     favorites = []
@@ -98,18 +97,22 @@ def urlback():
     response = requests.get(getinfourl)
     data = response.json()
     #print(data)
-    country=data['country_name']
-    region=data['region']
-    city=data['city']
+    try:
+        country=data['country_name']
+        region=data['region']
+        city=data['city']
+    except:
+        country = ''
+        region = ''
+        city = ''
     with open("viewed.txt", "a") as viewedtxt:
         viewedtxt.write(urlchosen)
     return render_template('home.html', video_feed=urlchosen, Country=country, Region=region, City=city)
 
 
-#Runs the server
 def start_server():
     app.run(host='127.0.0.1', port='5000', debug=False)
-
+    
 def keyboard_input():
     while True:
         if keyboard.read_key() == "d":
@@ -119,8 +122,14 @@ def keyboard_input():
         elif keyboard.read_key() == "f":
             window.load_url('http://127.0.0.1:5000/favorites/')
         else:
-            print('No Refresh')    
-    
+            print('No Refresh')
+
+def toggle_fullscreen(window):
+    window.hide()
+    time.sleep(8)
+    window.show()
+    window.toggle_fullscreen()
+
 if __name__ == '__main__':
     t = threading.Thread(target=start_server)
     t.daemon = True
@@ -128,5 +137,5 @@ if __name__ == '__main__':
     m = threading.Thread(target=keyboard_input)
     m.daemon = True
     m.start()
-    window = webview.create_window('Open Security Cameras Explorer', 'http://127.0.0.1:5000/data/', fullscreen=False)
-    webview.start()
+    window = webview.create_window('Open Security Cameras Explorer', 'http://127.0.0.1:5000/data/', text_select=False, minimized=True, resizable=False)
+    webview.start(toggle_fullscreen, window)
